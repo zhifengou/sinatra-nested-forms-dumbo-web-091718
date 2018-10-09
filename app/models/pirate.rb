1a60568c3e4b66a3f7f4e0840dcb098824629d01
zhifeng <zhifengou@hotmail.com>
2018-10-09 17:35:29 -0400

    built pirate nested forms

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+attr_accessor :name, :weight, :height, :ships
+@@all = []
+
+def self.all
+  @@all
+end
+
+# @ships = []
+
+def initialize(params)
+  @name = params[:name]
+  @weight = params[:weight]
+  @height = params[:height]
+  @ships = params[:ships]
+  @@all << self
+end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
+  def initialize(name,type,booty)
+    @name = name
+    @type = type
+    @booty = booty
+    @@all << self
+  end
+
+
 end
